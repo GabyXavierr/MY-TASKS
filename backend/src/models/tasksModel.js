eaf8484fb485b2c74d0b7fdbddf0361c94741f03
Gabriely Xavier <xgabriely00@gmail.com>
2024-05-27 22:18:44 -0300

    criando o banco de dados

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -2,8 +2,8 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const tasks = await connection.execute('SELECT * FROM tasks');
-  return tasks;
-};
+ return tasks;
+}
 
 module.exports= {
     getAll
