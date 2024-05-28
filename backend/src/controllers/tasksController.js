7d7eb1f66c6b59f497f826436ec8ff5e73bbe323
Gabriely Xavier <xgabriely00@gmail.com>
2024-05-27 23:43:13 -0300

    criando a primeira rota

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,10 +1,10 @@
 const tasksModel = require('../models/tasksModel');
 
-const getAll = async (request, response) => {
+const getAll = async (_request, response) => {
     const tasks = await tasksModel.getAll();
     return response.status(200).json(tasks);
 };
 
 module.exports = {
-    getAll
+    getAll,
 };
