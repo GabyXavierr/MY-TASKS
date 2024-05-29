afb0246d615375f247e26d81e7077d4f6e2ff5ce
Gabriely Xavier <xgabriely00@gmail.com>
2024-05-29 15:07:39 -0300

    criando o javascript do front

---
+++ frontend/js/script.js
@@ -0,0 +1,21 @@
+const fetchTasks = async () => {
+    const response = await fetch('http://localhost:3333/tasks');
+    const tasks = await response.json();
+    return tasks;
+};
+
+const createElement = (tag) => {
+const element = document.createElement(tag);
+return element;
+};
+
+const createRow = (task) => {
+
+    const {id, title, created_at, status} = task;
+
+    const tr = createElement('tr');
+    const tdTitle = createElement('td');
+
+    tdTitle.innerText = title;
+};
+
